e9135908c304901025c472b69c259a47ac0b3d0a
Phu Luong <phult.contact@gmail.com>
2020-05-06 01:09:00 +0700

    Optimize: get user agent

--- util/user-agent-util.js
+++ util/user-agent-util.js
@@ -23,6 +23,21 @@ function UserAgentUtil() {
         return retval;
     }
     this.getUserAgent = function (device) {
-        return userAgentStorage[device];
+        var retval = null;
+        switch (device) {
+            case "desktop": {
+                retval = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36";
+            }
+            case "mobile": {
+                retval = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
+            }
+            case "table": {
+                retval = "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1";
+            }
+            default: {
+                retval = userAgentStorage[device];
+            }
+        }
+        return retval;
     }
 }
