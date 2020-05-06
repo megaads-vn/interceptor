7e1254a7aaf82ec765d6ae6d518be01bb0376a50
Phu Luong <phult.contact@gmail.com>
2020-05-06 16:35:30 +0700

    Fix: tablet user agent

--- util/user-agent-util.js
+++ util/user-agent-util.js
@@ -31,7 +31,7 @@ function UserAgentUtil() {
             case "mobile": {
                 retval = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
             }
-            case "table": {
+            case "tablet": {
                 retval = "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1";
             }
             default: {
